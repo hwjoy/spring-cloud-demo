d5ff8bed4cceb965fe30ee2502e5555129b34a7c
hwjoy <hwjoy@163.com>
2020-08-30 12:00:05 +0800

    feat: add feigh-consumer

---
+++ feign-consumer/src/main/java/com/redant/feignconsumer/controller/ConsumerController.java
@@ -0,0 +1,23 @@
+package com.redant.feignconsumer.controller;
+
+import com.redant.feignconsumer.service.FeignProviderClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerController {
+
+    @Autowired
+    private FeignProviderClient feignProviderClient;
+
+    @GetMapping("/hello")
+    public String consumer() {
+        String results = feignProviderClient.hello();
+        return results;
+    }
+
+}
---
+++ feign-consumer/src/main/java/com/redant/feignconsumer/service/FeignProviderClient.java
@@ -0,0 +1,12 @@
+package com.redant.feignconsumer.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("EUREKA-CLIENT")
+public interface FeignProviderClient {
+
+    @GetMapping("/hello")
+    String hello();
+
+}
